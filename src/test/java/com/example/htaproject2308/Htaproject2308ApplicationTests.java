6f4963829ce17f03105a56fa6419e7c3de49aa09
incutv <incudev2@gmail.com>
2024-05-12 10:18:51 +0900

    project setting

---
+++ src/test/java/com/example/htaproject2308/Htaproject2308ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htaproject2308;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Htaproject2308ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
