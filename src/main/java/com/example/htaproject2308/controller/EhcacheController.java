000b4ea1a268175362683853bcff573f0b01abe3
sungyong_master <joosun3011@gmail.com>
2024-05-18 22:07:06 +0900

    카페인 캐시 적용

--- src/main/java/com/example/htaproject2308/controller/EhcacheController.java
+++ src/main/java/com/example/htaproject2308/controller/EhcacheController.java
@@ -4,6 +4,7 @@ import net.sf.ehcache.Element;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.ehcache.EhCacheCache;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
