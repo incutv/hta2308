e9305e29002a66b5fff091bd0bc305fb5c7f631f
kim san <rlatks15@naver.com>
2024-05-19 10:06:43 +0900

    emailservice에러로 인해 비활성화

--- src/main/java/com/example/htaproject2308/service/EmailService.java
+++ src/main/java/com/example/htaproject2308/service/EmailService.java
@@ -1,14 +1,11 @@
 package com.example.htaproject2308.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    @Autowired
+/*    @Autowired
     private JavaMailSender emailSender;
 
     public void sendSimpleMessage(String to, String subject, String text) {
@@ -17,5 +14,5 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
-    }
+    }*/
 }
