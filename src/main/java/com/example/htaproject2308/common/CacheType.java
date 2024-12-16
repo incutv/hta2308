bfef50717eb5862aeb47f3c46487e148446b5ddd
choijeonghyeon97 <cjh971202@gmail.com>
2024-12-17 01:51:46 +0900

    스프링 과제

--- src/main/java/com/example/htaproject2308/common/CacheType.java
+++ src/main/java/com/example/htaproject2308/common/CacheType.java
@@ -1,8 +1,14 @@
 package com.example.htaproject2308.common;
 
 import lombok.Getter;
-import org.checkerframework.framework.qual.RequiresQualifier;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CacheType {
+    NOTICE_FINDALL("NoticeReadMapper.findAll", 10, 10000);
+
+    private final String cacheName;
+    private final int expiredAfterWrite;
+    private final int maximumSize;
 }
