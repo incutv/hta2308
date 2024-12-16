622612857513ae4bfbe77d1099b0ce68070d40e6
jhShin <sinmagic123@gmail.com>
2024-12-16 23:05:16 +0900

    캐시 끝

---
+++ src/main/java/com/example/htaproject2308/common/CacheType.java
@@ -0,0 +1,17 @@
+package com.example.htaproject2308.common;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CacheType {
+    NOTICE_FINDALL("NoticeReadMapper.findTop10Views", 10, 10000);
+
+    private final String cacheName;
+    private final int expiredAfterWrite;
+    private final int maximumSize;
+
+
+}
