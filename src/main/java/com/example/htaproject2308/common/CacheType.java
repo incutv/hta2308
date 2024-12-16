45e136d3535a37db698077f6eda804050dc8604e
jhShin <sinmagic123@gmail.com>
2024-12-16 23:23:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/htaproject2308/common/CacheType.java
@@ -0,0 +1,15 @@
+package com.example.htaproject2308.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CacheType {
+
+    NOTICE_FINDALL("NoticeMapper.findAll", 10, 10000);
+
+    private final String cacheName;
+    private final int expiredAfterWrite;
+    private final int maximumSize;
+}
